3527c522e784a02b30d38408971717c2a0641c88
Michael Burns <mjburns@techknowledg.ie>
2021-03-03 19:15:52 +0000

    change icon sizes

--- frontend.py
+++ frontend.py
@@ -4,7 +4,6 @@ from tkinter import *
 from tkinter import ttk
 from sqlite3 import Error
 import sqlite3
-import pandas as pd
 import PIL.ImageTk
 import PIL.Image
 import database
@@ -31,8 +30,16 @@ def getTodo():
 
     number_of_todos = len(activities)
 
+    my_activities = []
+
+    for activity in activities:
+        my_activity = str(activity)
+        my_activity = my_activity[2:]
+        my_activity = my_activity[:-3]
+        my_activities.append(my_activity)
+
     for i in range(number_of_todos):
-        ttk.Label(content, text=activities[i]).grid(column=0, row=i+1)
+        ttk.Label(content, text=my_activities[i]).grid(column=0, row=i+1)
 
         if (completed[i] == (0,)):
             control = 0
@@ -74,31 +81,30 @@ def getNews():
     todays_news = news.get()
 
     for index, item in enumerate(todays_news):
-        ttk.Label(content, text=item).grid(column=0, row=index+1)
+        ttk.Label(content, text=item).grid(column=0, row=index+1, sticky=(W,))
 
 def getWeather():
     destroyChildren()
     
-    ttk.Label(content, text="Liverpool").grid(column=0, row=0)
+    ttk.Label(content, text="Liverpool").grid(column=0, row=0, rowspan=1)
 
     todays_weather = weather.get()
 
     for index, data in enumerate(todays_weather):
-        ttk.Label(content, text=data).grid(column=0, row=index+1)
+        ttk.Label(content, text=data).grid(column=0, row=index+1, sticky=(W,))
 
-    ttk.Label(content, image=img_todays_weather).grid(column=1, row=1)
+    ttk.Label(content, image=img_todays_weather).grid(column=1, row=1, columnspan=3)
 
 def getGoodreads():
     destroyChildren()
 
     data = goodreads.get()
 
-    ttk.Label(content, text="Goodreads").grid(column=0, row=0)
     ttk.Label(content, text=data[0]).grid(column=0, row=1)
     ttk.Label(content, text="Recently read").grid(column=0, row=2)
     
     for i in range(1, 6):
-        ttk.Label(content, text=data[i]).grid(column=0, row=i+2)
+        ttk.Label(content, text=data[i]).grid(column=0, row=i+2, sticky=(W,))
 
 root = Tk()
 root.title("Pi R-Squared")
@@ -108,27 +114,28 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-content = ttk.Frame(mainframe, height="520", width="720")
+content = ttk.Frame(mainframe, height=520, padding=(50,25,50,25), width=720)
 content.grid(column=0, row=0, sticky=(N, W, E))
 
-buttons = ttk.Frame(mainframe, height="200", width="720")
+buttons = ttk.Frame(mainframe, height=200, width=720)
 buttons.grid(column=0, row=1, sticky=(W, E, S))
 
-img_todo = PhotoImage(file="icons/todo.png")
-img_news = PhotoImage(file="icons/news.png")
-img_weather = PhotoImage(file="icons/weather.png")
-img_goodreads = PhotoImage(file="icons/goodreads.png")
+img_todo = PhotoImage(file="icons/todo_90.png")
+img_news = PhotoImage(file="icons/news_90.png")
+img_weather = PhotoImage(file="icons/weather_90.png")
+img_goodreads = PhotoImage(file="icons/goodreads_90.png")
 img_checked = PhotoImage(file="icons/checked.png")
 img_unchecked = PhotoImage(file="icons/unchecked.png")
 img_todays_weather = PhotoImage(file="icons/todays_weather.png")
+logo = PhotoImage(file="icons/goodreads_logo.png")
 
-button_todo = ttk.Button(buttons, command=getTodo, image=img_todo).grid(column=0, row=0, sticky=(N, W, S))
-ttk.Button(buttons, command=getNews, image=img_news).grid(column=1, row=0, sticky=(N, S))
+button_todo = ttk.Button(buttons, command=getTodo, image=img_todo ).grid(column=0, row=0, sticky=(N, W, S))
+ttk.Button(buttons, width=25, command=getNews, image=img_news).grid(column=1, row=0, sticky=(N, S))
 ttk.Button(buttons, command=getWeather, image=img_weather).grid(column=2, row=0, sticky=(N, S))
 ttk.Button(buttons, command=getGoodreads, image=img_goodreads).grid(column=3, row=0, sticky=(N, E, S))
 
 for child in buttons.winfo_children():
-    child.grid_configure(padx=50, pady=20)
+    child.grid_configure(padx=30, pady=30)
 
 getTodo()
 
